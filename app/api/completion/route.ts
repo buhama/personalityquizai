e96aed082d33f0fbc8607acf900779369e2dbdd7
buhama <hassan@hassanahmed.ca>
2023-07-29 19:52:40 -0400

    we did it

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -28,38 +28,14 @@ export async function POST(req: Request) {
 						question: {
 							type: 'string',
 							description: 'The question to ask the user.',
+							required: ['question'],
 						},
 						options: {
 							type: 'array',
 							description: 'List of options to choose from.',
 							items: {
-								type: 'object',
-								properties: {
-									option: {
-										type: 'string',
-										description: 'The option to choose from.',
-									},
-									points: {
-										type: 'array',
-										description: 'List of points to assign to each result.',
-										items: {
-											type: 'object',
-											properties: {
-												result: {
-													type: 'string',
-													description: 'The result to assign points to.',
-												},
-												points: {
-													type: 'number',
-													description:
-														'The number of points to assign to the result.',
-												},
-											},
-											required: ['result', 'points'],
-										},
-									},
-								},
-								required: ['option', 'points'],
+								type: 'string',
+								required: ['option'],
 							},
 						},
 					},
@@ -75,7 +51,7 @@ export async function POST(req: Request) {
 		messages: [
 			{
 				role: 'system',
-				content: `{\n\trole: 'system',\n\tcontent:\n\t\t\"You're going to help me generate personality quizzes for my personality quizzes website. You will be given a prompt and you will generate ${numberOfQuestions} questions for that prompt. Each question will be multiple choice with 4 options. Each question should be something tells you something about that persons personality, be creative. You can create scenarios, ask for preferences, or more. Then depending on the users choices, I will assign a result to the user. Each answer should assign a points value to one of the potential results. The questions JSON should include how many points go towards each result based on the answer Create both the questions and answers JSON for this prompt. Here are the types \\n\\ninterface Questions {\\n   question: string;\\n   options: Options[];\\n}\\n\\ninterface Options {\\n   option: string;\\n   points: { result: Result, points: number }[];\\n}\"\n}`,
+				content: `{\n\trole: 'system',\n\tcontent:\n\t\t\"You're going to help me generate personality quizzes for my personality quizzes website. You will be given a prompt and you will generate ${numberOfQuestions} questions for that prompt. Each question will be multiple choice with 4 options. Each question should be something tells you something about that persons personality, be creative. You can create scenarios, ask for preferences, or more`,
 			},
 			{
 				role: 'system',
